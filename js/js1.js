e6ed56e28b24fae659f96deb476000dd030830a4
wangzeyuan <739077130@qq.com>
2016-07-12 17:08:36 +0800

    Signed-off-by: wangzeyuan <739077130@qq.com>

---
+++ js/js1.js
@@ -0,0 +1,17 @@
+(
+	function()
+	   		window.onload=function(){
+		
+//	 			document.getE{
+//				if(this.innerHTML =="关灯"){
+//					document.body.style.backgroundColor="black";
+//					btn.innerHTML ="开灯";
+//			}
+//			else{
+//				document.body.style.backgroundColor="white";
+//				btn.innerHTML ="关灯";
+//			}
+            for(var i=0;i<10;i++)
+            
+			}
+)
