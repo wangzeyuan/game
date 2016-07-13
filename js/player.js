1f2a4101b2d4e012ee01638f166045f6a96bf1f5
wangzeyuan <739077130@qq.com>
2016-07-13 15:27:46 +0800

    Signed-off-by: wangzeyuan <739077130@qq.com>

---
+++ js/player.js
@@ -0,0 +1,50 @@
+var users = [{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:4
+    },{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:1
+	},{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:1
+	},{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:1
+	},{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:1
+	},{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:1
+	},{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:1
+	},{
+		username : "玩家一",
+		id:null,
+		total:0,
+		win:0,
+		line:2
+	}
+]
