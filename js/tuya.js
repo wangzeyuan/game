75bce61600add60ded7329d18d277bbd042c7c1c
wangzeyuan <739077130@qq.com>
2016-07-16 16:16:55 +0800

    Signed-off-by: wangzeyuan <739077130@qq.com>

--- js/tuya.js
+++ js/tuya.js
@@ -5,25 +5,84 @@ var offset = 0;
 
 $(function(){
 	pen = $("#can").get(0).getContext("2d");
+	pen.strokeStyle = "black";
 	offset = $("#can").offset();
 	
-	$("#can").mousedown(function( event ){
-		isdraw = true;
-		pen.moveTo(event.clientX-offset.left,event.clientY-offset.top);
+	drawPen();
+	
+	$("#pen").click(function(){
+		drawPen();
 	});
 	
-	$("#can").mousemove(function( event ){
+	$("#line").click(function(){
+		drawLine();
+	});
+	
+	$("#rect").click(function(){
+		drawRect();
+	});
+	
+	$("#linewidth").change(function(){
+		pen.lineWidth = $(this).val();		
+	});
+	
+	$("#color").change(function(){
+		pen.strokeStyle = $(this).val();
+	});
+});
+
+function drawRect(){
+	var x = y = 0;
+	$("#can").unbind().mousedown(function( event ){
+		isdraw = true;
+		pen.beginPath();
+		x = event.clientX-offset.left;
+		y = event.clientY-offset.top;
+	}).mouseup(function(event){
+		if(isdraw){
+			pen.rect(x,y,event.clientX-offset.left-x,event.clientY-offset.top-y);
+			pen.stroke();
+			pen.closePath();
+		}
+		isdraw = false;
+	}).mouseleave(function(event){
+		isdraw = false;
+	});	
+}
+
+function drawLine(){
+	$("#can").unbind().mousedown(function( event ){
+		isdraw = true;
+		pen.beginPath();
+		pen.moveTo(event.clientX-offset.left,event.clientY-offset.top);
+	}).mouseup(function(event){
 		if(isdraw){
 			pen.lineTo(event.clientX-offset.left,event.clientY-offset.top);
 			pen.stroke();
+			pen.closePath();
 		}
-	});
-	
-	$("#can").mouseup(function(event){
+		isdraw = false;
+	}).mouseleave(function(event){
 		isdraw = false;
 	});
-	
-	$("#can").mouseleave(function(event){
+}
+
+function drawPen(){
+	$("#can").unbind().mousedown(function( event ){
+		isdraw = true;
+		pen.beginPath();
+		pen.moveTo(event.clientX-offset.left,event.clientY-offset.top);
+	}).mousemove(function( event ){
+		if(isdraw){
+			
+			pen.lineTo(event.clientX-offset.left,event.clientY-offset.top);
+			pen.stroke();
+			
+		}
+	}).mouseup(function(event){
+		isdraw = false;
+		pen.closePath();
+	}).mouseleave(function(event){
 		isdraw = false;
 	});
-});
+}
